881306e98a4e54d14c30a81b91476b229e091d95
sanketpatil05 <patilsanket209@gmail.com>
2022-11-17 14:49:52 +0530

    added

--- sugarcosmetics/src/App.js
+++ sugarcosmetics/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+
+
+
+    <h1>Suggar Cosmetciss</h1>
+  </div>;
 }
 
 export default App;
