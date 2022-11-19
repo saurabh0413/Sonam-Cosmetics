2dae542c2e4d8abc4746a563f62cf8513b97d0e6
sanketpatil05 <patilsanket209@gmail.com>
2022-11-19 13:31:30 +0530

    added second navbar

--- sugarcosmetics/src/App.js
+++ sugarcosmetics/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import {Navbar} from './Components/Navbar';
+import SecondNabar from './Components/SecondNabar';
 
 function App() {
   return (
+    <>
    <Navbar/>
+   <SecondNabar/>
+    </>
+  
   );
 }
 
--- sugarcosmetics/src/Components/SecondNabar.jsx
+++ sugarcosmetics/src/Components/SecondNabar.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import { ReactNode } from 'react';
+import {
+  
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  color,
+} from '@chakra-ui/react';
 
-export const SecondNabar = () => {
+
+
+export default function SecondNabar() {
+  
+
   return (
-    <div>SecondNabar</div>
-  )
+    <>
+      
+          
+      <Tabs style={{backgroundColor:"black", color:"white"}}>
+  <TabList>
+    <Tab>Makeup</Tab>
+    <Tab>Brushes</Tab>
+    <Tab>Skincare</Tab>
+    <Tab>Gifting</Tab>
+    <Tab>Blog</Tab>
+    <Tab>Offers</Tab>
+    <Tab>Stores</Tab>
+  </TabList>
+
+  <TabPanels>
+    <TabPanel>
+      <p>one!</p>
+    </TabPanel>
+    <TabPanel>
+      <p>two!</p>
+    </TabPanel>
+    <TabPanel>
+      <p>three!</p>
+    </TabPanel>
+  </TabPanels>
+</Tabs>
+          
+      
+        
+     
+    </>
+  );
 }
