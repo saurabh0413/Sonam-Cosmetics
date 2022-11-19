ba61dd368eb7e8c6a14851dd2088f4de91d230be
saurabh <saurabhfarkade83@gmail.com>
2022-11-19 20:35:08 +0530

    filter-section-completed

--- sugarcosmetics/src/Components/ProductsData/Filter.jsx
+++ sugarcosmetics/src/Components/ProductsData/Filter.jsx
@@ -1,7 +1,131 @@
 import React from "react";
-
+import { useSearchParams } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+  VStack,
+  Checkbox,
+  CheckboxGroup,
+} from "@chakra-ui/react";
+import AccordCompo from "./AccordCompo";
 const Filter = () => {
-  return <div style={{ border: "2px solid red", width:"25%" }}>Filter</div>;
+  const data = [
+    {
+      productType: [
+        "Product Type",
+        "primers",
+        "powder",
+        "Translucent Powder",
+        "compact",
+        "Foundations & Concealers",
+        "Face Foundation",
+        "BB Cream",
+        "stick",
+        "palette",
+        "Pressed Powder",
+        "value sets",
+        "",
+      ],
+    },
+    {
+      productType: [
+        "Feature",
+        "primer",
+        "Translucent Powder",
+        "Face Foundation",
+        "stick",
+        "",
+      ],
+    },
+    {
+      productType: [
+        "Formulation",
+        "cream",
+        "Loose Powder",
+        "Pressed Powder",
+        "stick",
+        "Liquid",
+      ],
+    },
+    {
+      productType: [
+        "Concern",
+        "Brightening",
+        "oil control",
+        "under eye",
+        "Blurring",
+      ],
+    },
+  ];
+  const [searchParams, setSearchParams] = useSearchParams();
+  const initialGenreparams = searchParams.getAll("type");
+
+  const [category, setCategory] = useState(initialGenreparams || []);
+
+  const handleChange = (e) => {
+    const checkedData = e.target.value;
+    const new_category = [...category];
+
+    if (category.includes(checkedData)) {
+      new_category.splice(new_category.indexOf(checkedData), 1);
+    } else {
+      new_category.push(checkedData);
+    }
+    setCategory(new_category);
+  };
+  useEffect(() => {
+    console.log("filter");
+    if (category) {
+      const params = {};
+      category && (params.type = category);
+      setSearchParams(params);
+    }
+  }, [category, setSearchParams]);
+  return (
+    <div style={{ border: "2px solid red", width: "25%" }}>
+      <Box w={300} ml={10} mb={10}>
+        <Accordion allowToggle>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box flex="1" textAlign="left">
+                  Sort By: Relevance
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              <CheckboxGroup colorScheme="green">
+                <VStack
+                  spacing={[1, 5]}
+                  direction={["column", "row"]}
+                  align="flex-start"
+                >
+                  <Checkbox value="Relevance">Relevance</Checkbox>
+                  <Checkbox value="High">Price: High to Low</Checkbox>
+                  <Checkbox value="Low">Price: Low to High</Checkbox>
+                </VStack>
+              </CheckboxGroup>
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </Box>
+
+      <Box w={300} ml={10}>
+        <Accordion allowToggle>
+          {data.map((item) => {
+            return <AccordCompo item={item} handleChange={handleChange} category={category} />;
+          })}
+        </Accordion>
+      </Box>
+    </div>
+  );
 };
 
 export default Filter;
--- sugarcosmetics/src/index.js
+++ sugarcosmetics/src/index.js
@@ -4,16 +4,20 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
-import ProductContext  from "./context/productContext";
+import ProductContext from "./context/productContext";
+import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   // <React.StrictMode>
-    <ProductContext>
+  <ProductContext>
+    <BrowserRouter>
       <ChakraProvider>
         <App />
       </ChakraProvider>
-    </ProductContext>
+    </BrowserRouter>
+  </ProductContext>
+
   // </React.StrictMode>
 );
 
