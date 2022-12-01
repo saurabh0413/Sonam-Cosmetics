6c102d15de45cd95a61437e05fc53f076fff0d0c
saurabh <saurabhfarkade83@gmail.com>
2022-12-01 23:45:42 +0530

    navbar-added

--- sugarcosmetics/src/Components/ProductsData/Products.jsx
+++ sugarcosmetics/src/Components/ProductsData/Products.jsx
@@ -3,22 +3,25 @@ import Filter from "./Filter";
 import ProductContent from "./ProductContent";
 import { Flex, Stack, Wrap } from "@chakra-ui/react";
 import styles from "./Products.module.css";
+import Navbar from "../Navbar/Navbar";
 const Products = () => {
   return (
     <div>
-      <div style={{ width: "100%", height: "150px" }}>
+      <Navbar />
+      {/* <div style={{ width: "100%", height: "150px" }}>
         <img
           src="https://imgs.search.brave.com/LcVUbpfe1IagdSCXE8XJx33HLB6ztIBN2rnDhwycpRs/rs:fit:844:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC52/X3lfU09BTmtiMWhv/M1dLNkxIYnFnSGFF/SyZwaWQ9QXBp"
           alt=""
           className={styles.img1}
         />
-      </div>
-
+      </div> */}
+ //base sm md lg xl 2xl
       <Stack
         justify="space-between"
         maxW="1400px"
         margin="auto"
-        direction={{ base: "column",md:"row" }} //
+        // direction={{ base: "column", md: "row" }} //
+        direction="row"
       >
         <Filter />
         <ProductContent />
--- sugarcosmetics/src/Pages/Singleproduct.jsx
+++ sugarcosmetics/src/Pages/Singleproduct.jsx
@@ -1,11 +1,23 @@
 import styled from "styled-components";
-import React from "react";
+import React, { useState } from "react";
 import { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { ProdContext } from "../context/productContext";
-import { Box, HStack, ListItem, Stack, Text, UnorderedList } from "@chakra-ui/react";
-import { HamburgerIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Divider,
+  HStack,
+  Image,
+  ListItem,
+  Stack,
+  Text,
+  UnorderedList,
+  VStack,
+  Wrap,
+} from "@chakra-ui/react";
+import { ChevronDownIcon, HamburgerIcon } from "@chakra-ui/icons";
 import Cartbutton from "../Components/ProductsData/Cartbutton";
+
 const Singleproduct = () => {
   const getproduct = useParams();
   console.log(getproduct.title);
@@ -15,43 +27,50 @@ const Singleproduct = () => {
   );
   console.log(state.products);
   console.log(new_product, "new");
-  return (
-    <div>
-      <Stack>
-        <Box>this is for images</Box>
-        <Box w="50%" border="2px solid red" ml="40">
-          <Text fontSize="2xl">{new_product.name}</Text>
-          <Text mt="10" mb={10}>
-            {new_product.price}
-          </Text>
-          <Text as="b" ml={10}>
-            Available Offers
-          </Text>
-          <UnorderedList spacing={3} ml={20}>
-            <ListItem>
-              Get A Matte Attack Transferproof Lipstick - 06 Spring Crimson
-              (Crimson Red) And A Flat Glitter Gold Pouch Free On A Minimum
-              Spend Of Rs. 799
-            </ListItem>
-            <Text>Know more</Text>
-            <ListItem>
-              Grab A Complimentary Product Worth Rs.3000 On A Spend Of Rs.4999
-            </ListItem>
-            <Text>Know more</Text>
-          </UnorderedList>
 
-          <Text color="red" ml={16} mt={6}>
-            +view more
+  const [centerimg, SetCenterimg] = useState(new_product.images[0]);
+  return (
+    <Stack
+      direction={{ base: "column", sm: "row" }}
+      justify="center"
+     
+      spacing={50}
+      border="2px solid red"
+    >
+      <HStack spacing={10}>
+        <VStack spacing={5}>
+          {new_product.images.map((img) => {
+            return <Image w="50px" src={img} />;
+          })}
+        </VStack>
+        <Image src={centerimg} w="300px" minW="80px" />
+      </HStack>
+
+      <VStack
+        align="revert-layer"
+        spacing={{ base: "none", sm: 3, xl: 5 }}
+        w={{ base: "100%", sm: "200px", xl: "500px" }}
+      >
+        <Text>{new_product.name}</Text>
+        <Text>{new_product.price}</Text>
+        <VStack align="revert-layer" spacing={5}>
+          <Text>AVAILABLE OFFERS!!</Text>
+          <Text overflow="hidden">
+            Get A Matte Attack Transferproof Lipstick - 06 Spring Crimson
+            (Crimson Red) On A Minimum Spend Of Rs. 799
           </Text>
-          <Box mt={20} ml={20}>
-            <HStack spacing="60px">
-              <HamburgerIcon />
-              <Cartbutton />
-            </HStack>
-          </Box>
+          <Text overflow="hidden">
+            Grab A Complimentary Product Worth Rs.3000 On A Spend Of Rs.499
+          </Text>
+        </VStack>
+        <Box>
+          <HStack spacing="60px">
+            <HamburgerIcon />
+            <Cartbutton />
+          </HStack>
         </Box>
-      </Stack>
-    </div>
+      </VStack>
+    </Stack>
   );
 };
 
